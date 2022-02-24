eb1d0609adac765c66f8ff2f3359106c8b89c485
Rachel PR <rplourder@gmail.com>
2022-02-24 10:41:05 -0400

    Add some extra error handling for response.statusCode

--- iss.js
+++ iss.js
@@ -13,11 +13,24 @@ const fetchMyIP = function (callback) {
 
   url = "https://api.ipify.org/?format=json"
   request(url, (error, response, body) => {
+    
+    // error can be set if invalid domain, user is offline, etc.
+    if (error){
+      callback(error,null);
+      return;
+    }
+    
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200){
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg),null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
     const ip = JSON.parse(body).ip;
-    console.log(ip)
-    
-    
-  });
+    callback(null,ip)
+  });  
 };
 
 module.exports = { fetchMyIP }
