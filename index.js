9d12a61afaac95fa5fcc09e6a07f526749e45087
Rachel PR <rplourder@gmail.com>
2022-02-25 12:20:11 -0400

    Add temporary test code to check that the fetchISSFlyOverTimes function works with given coordinates.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //A file that contains most of the logic for fetching the data from each API endpoint.
 
 
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 //This code was temporary to test out fetchMyIP function
 // fetchMyIP((error, ip) => {
@@ -12,10 +12,20 @@ const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 //   console.log("Found your IP: ", ip)
 // });
 
-// fetchCoordsByIP("24.224.233.49",(error, coordinates) => {
-//   if (error){
+// fetchCoordsByIP("24.224.233.49", (error, coordinates) => {
+//   if (error) {
 //     console.log("It didn't work!", error)
 //     return;
 //   }
-//   console.log("It worked! Returned coordinates:",coordinates)
+//   console.log("It worked! Returned coordinates:", coordinates)
 // });
+
+const testCoords = { latitude: '49.27670', longitude: '-123.13000' }
+
+fetchISSFlyOverTimes(testCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error)
+    return;
+  }
+  console.log("It worked!, it'll be overhead at", passTimes)
+});
