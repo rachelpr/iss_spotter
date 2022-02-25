57ae59ef9de9bd22b99edd711e39ade5da88f3f5
Rachel PR <rplourder@gmail.com>
2022-02-24 22:43:02 -0400

    Add some temporary code for testing fetchMyCoords.js func

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //A file that contains most of the logic for fetching the data from each API endpoint.
 
 
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 //This code was temporary to test out fetchMyIP function
 // fetchMyIP((error, ip) => {
 //   if (error) {
-//     console.log("Couldn't find the IP. ", error);
+//     console.log("Couldn't find the IP.", error);
 //     return;
 //   }
 //   console.log("Found your IP: ", ip)
+// });
+
+// fetchCoordsByIP("24.224.233.49",(error, coordinates) => {
+//   if (error){
+//     console.log("It didn't work!", error)
+//     return;
+//   }
+//   console.log("It worked! Returned coordinates:",coordinates)
 // });
